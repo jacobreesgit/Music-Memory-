29435ed9ac3cf9cf36400007f3e47de9ad0b501a
jacobreesgit <jacobrees@me.com>
2025-06-01 19:43:38 +0100

    Initial Commit

---
+++ Music Memory/Music_MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_MemoryApp.swift
+//  Music Memory
+//
+//  Created by Jacob Rees on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Music_MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
